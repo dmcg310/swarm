b719c274e7e38c56f465e80da686c3dec442e463
Darragh McGurk <120114728+dmcg310@users.noreply.github.com>
2026-08-22 22:08:46 +0100

    refactor: some tidying

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,21 @@ struct Particle {
     pos: Vec2,
     vel: Vec2,
     radius: f32,
+    color: Color,
 }
 
 impl Particle {
-    fn new(pos: Vec2, vel: Vec2, radius: f32) -> Self {
-        Self { pos, vel, radius }
+    fn new(pos: Vec2, vel: Vec2, radius: f32, color: Color) -> Self {
+        Self {
+            pos,
+            vel,
+            radius,
+            color,
+        }
     }
 }
 
-const N: usize = 100;
+const N: usize = 10_000;
 
 #[macroquad::main(window_conf)]
 async fn main() {
@@ -27,20 +33,14 @@ async fn main() {
     }
 }
 
+// -- Main loop functions
+
 fn input() {}
 
 fn update(particles: &mut Vec<Particle>) {
     // O(n) update of all particles
     for p in particles.iter_mut() {
-        p.pos += p.vel;
-
-        // Simulate "bouncing" off the walls by inverting the velocity when hitting a wall
-        if p.pos.x < 0.0 || p.pos.x > screen_width() {
-            p.vel.x *= -1.0;
-        }
-        if p.pos.y < 0.0 || p.pos.y > screen_height() {
-            p.vel.y *= -1.0;
-        }
+        update_particle(p);
     }
 }
 
@@ -48,12 +48,37 @@ fn render(particles: &Vec<Particle>) {
     clear_background(DARKGRAY);
 
     for p in particles.iter() {
-        draw_circle(p.pos.x, p.pos.y, p.radius, WHITE);
+        draw_particle(p);
     }
 
+    draw_metrics();
+}
+
+// -- Update functions
+
+fn update_particle(p: &mut Particle) {
+    p.pos += p.vel;
+
+    // Simulate "bouncing" off the walls by inverting the velocity when hitting a wall
+    if p.pos.x < 0.0 || p.pos.x > screen_width() {
+        p.vel.x *= -1.0;
+    }
+    if p.pos.y < 0.0 || p.pos.y > screen_height() {
+        p.vel.y *= -1.0;
+    }
+}
+
+// -- Draw functions
+
+fn draw_particle(p: &Particle) {
+    // A particle is just a cricle at the end of the day
+    draw_circle(p.pos.x, p.pos.y, p.radius, p.color);
+}
+
+fn draw_metrics() {
     draw_text(
         &format!(
-            "fps: {:.2} - frame_time: {:.4}s - particles: {}",
+            "fps: {:.2}, frame_time: {:.4}s, particles: {}",
             macroquad::time::get_fps(),
             macroquad::time::get_frame_time(),
             N
@@ -61,18 +86,23 @@ fn render(particles: &Vec<Particle>) {
         20.0,
         30.0,
         30.0,
-        WHITE,
+        YELLOW,
     );
 }
 
+// -- Helper functions
+
 fn create_particles(n: usize) -> Vec<Particle> {
-    // perf: we know the size of the vec
+    let radius = 2.0;
+
+    // Perf: we know the size of the vec
     let mut particles = Vec::with_capacity(n);
     for _ in 0..n {
         particles.push(Particle::new(
             get_random_pos(),
             get_random_vel(),
-            get_random_radius(),
+            radius,
+            get_random_color(),
         ));
     }
 
@@ -93,17 +123,28 @@ fn get_random_vel() -> Vec2 {
     vec2(rand::gen_range(low, high), rand::gen_range(low, high))
 }
 
-fn get_random_radius() -> f32 {
-    let low = 5.0;
-    let high = 15.0;
-    rand::gen_range(low, high)
+fn get_random_color() -> Color {
+    let low = 0.0;
+    let high = 1.0;
+    let alpha = 1.0;
+    Color::new(
+        rand::gen_range(low, high),
+        rand::gen_range(low, high),
+        rand::gen_range(low, high),
+        alpha,
+    )
 }
 
+// -- Window configuration
+
 fn window_conf() -> Conf {
+    let title = String::from("swarm");
+    let width = 1280;
+    let height = 720;
     Conf {
-        window_title: String::from("swarm"),
-        window_width: 1280,
-        window_height: 720,
+        window_title: title,
+        window_width: width,
+        window_height: height,
         ..Default::default()
     }
 }
